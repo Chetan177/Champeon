215f464e0f298f72915764d73926d50d8b001800
Chetan Pandey <36603558+Chetan177@users.noreply.github.com>
2019-07-24 13:28:07 +0530

    Add files via upload

--- PUBG Prediction system/main.py
+++ PUBG Prediction system/main.py
@@ -23,8 +23,9 @@ def predict():
     data = get_data_to_predict(user_name)
     
     res = predict_res(data,model)
-    res = float("{0:.6f}".format(res))
-    res = str(res*100) + "%  Win Chance" 
+    res= res *100
+    res = float("{0:.4f}".format(res))
+    res = "You have "+ str(res) + "%  Chance Winning the next match" 
 
     return render_template("index.html",output = res)
 
