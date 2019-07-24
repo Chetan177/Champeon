6e494ad2bb4119b7e8cc810c5506ab8535b6afa9
Chetan Pandey <36603558+Chetan177@users.noreply.github.com>
2019-07-24 15:08:37 +0530

    Add files via upload

--- PUBG Prediction system/main.py
+++ PUBG Prediction system/main.py
@@ -21,7 +21,6 @@ def predict():
     user_name = request.form['userId']
     print(user_name)
     data = get_data_to_predict(user_name)
-    
     res = predict_res(data,model)
     res= res *100
     res = float("{0:.4f}".format(res))
@@ -29,9 +28,13 @@ def predict():
 
     return render_template("index.html",output = res)
 
+@app.errorhandler(500)
+def internal_error(error):
+
+    return render_template("index.html",output = "Please provide a correct user ID !!!")
 
 
 
 if __name__ == "__main__":
     model = load()
-    app.run() # app.run(host="0.0.0.0")
+    app.run(port =5050) # app.run(host="0.0.0.0")
